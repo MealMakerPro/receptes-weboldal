5af1da635bf2bb2580d7c20c1127f383b59e6c5f
Mészáros-Jakus Nikolett <h657409@stud.u-szeged.hu>
2025-02-27 11:58:41 +0100

    Működő regisztráció feltöltése

--- javascript/index.js
+++ javascript/index.js
@@ -2,28 +2,25 @@ import { handleRegistration } from "./registration";
 import {handleLogIn} from "./login";
 import "../css/all_pages.css";
 import header from "../img/name.png";
+import {signInWithEmailAndPassword} from "firebase/auth";
 
 document.getElementById('headerImg').src = header;
 
-document.addEventListener("DOMContentLoaded", handleRegistration);
-document.addEventListener("DOMContentLoaded", handleLogIn);
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.getElementById("registrationForm");
+    if (form) {
+        form.addEventListener("submit", (e) => {
+            e.preventDefault();
+            const name = document.getElementById("name").value;
+            const email = document.getElementById("email").value;
+            const username = document.getElementById("username").value;
+            const password = document.getElementById("password").value;
 
-document.getElementById("registrationForm").addEventListener("submit", (e) => {
-    e.preventDefault();
+            handleRegistration(name, email, username, password);
+            setTimeout(() => {
+                location.reload();
+            }, 500);
+        });
+    }
+});
 
-    const name = document.getElementById("name").value;
-    const username = document.getElementById("username").value;
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
-
-    handleRegistration(name, email, username, password);
-})
-
-document.getElementById("loginForm").addEventListener("submit", (e) => {
-    e.preventDefault();
-    
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
-    
-    handleLogIn(email, password);
-})
